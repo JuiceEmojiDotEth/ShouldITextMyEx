61f11f113873c67e3b6bf092d83052fcc765ee3f
JuiceEmojiDotEth <165119846+JuiceEmojiDotEth@users.noreply.github.com>
2024-06-01 18:05:23 -0400

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -42,6 +42,7 @@ document.getElementById("yesButton").addEventListener("click", function() {
 
     // Play the circus music if it has not been played yet
     if (!circusMusicPlayed) {
+        circusMusic.currentTime = 8; // Start X seconds into the song
         circusMusic.play();
         circusMusicPlayed = true; // Set the flag to true after playing
     }
